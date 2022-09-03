597f378cf37d9f09d9c378a989360c804b2c4a20
Paywand Mohammed Hussein <73426000+paywand007@users.noreply.github.com>
2022-09-03 11:26:53 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Add your code here
+function submitData(email, name) {
+    return fetch('http://localhost:3000/users', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                Accept: 'application/json',
+            },
+            body: JSON.stringify({
+                name,
+                email,
+            }),
+        })
+        .then((response) => response.json())
+        .then((object) => (document.body.innerHTML = object['id']))
+        .catch((error) => (document.body.innerHTML = error.message))
+}
